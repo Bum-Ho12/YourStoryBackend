0002168bb262641a922f5cd09a207001a22ecbf5
Bum-Ho12 <bumhonisu@gmail.com>
2023-11-26 14:19:34 +0300

    refactor: extra security

--- YourStoryBackend/settings.py
+++ YourStoryBackend/settings.py
@@ -9,22 +9,24 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%+*cs@$+l3*=8(v-1uut**-spu2hb^sg(-gv1_0)d1zjc+s@)-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'api.Account'
 
 
