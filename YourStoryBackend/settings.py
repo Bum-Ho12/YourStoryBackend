7765664b5441a49d6a1fe65abe6c64e58394e231
Bum-Ho12 <bumhonisu@gmail.com>
2023-11-27 17:03:53 +0300

    refactor: cors issue

--- YourStoryBackend/settings.py
+++ YourStoryBackend/settings.py
@@ -23,10 +23,13 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 AUTH_USER_MODEL = 'api.Account'
 
 
