8662058c757fd0f2e41cac60fd75e890bd540f2f
Bum-Ho12 <bumhonisu@gmail.com>
2023-12-06 05:46:14 +0300

    vercel update

--- YourStoryBackend/wsgi.py
+++ YourStoryBackend/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+# import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# if os.environ.get('ENV') == 'production':
+#     sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YourStoryBackend.settings')
 
 application = get_wsgi_application()
+
+app = application # added for vercel
